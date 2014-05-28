26ee7be1a04f16a5da573f07daa03656b51074f3
“willjasen” <willjasen@gmail.com>
2014-05-28 15:35:33 -0400

    removing omniauth.rb

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-provider :google_oauth2, ENV["GLASS_CLIENT_ID"], ENV["GLASS_CLIENT_SECRET"], {
- access_type: 'offline',
- prompt: 'consent',
- scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/glass.timeline',
- redirect_uri:'https://willjasen-dg.ngrok.com/auth/google_oauth2/callback'
- }
-end
